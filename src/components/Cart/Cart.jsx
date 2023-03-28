ca5cfa63a6509c14807944b68968aaaa3dae64f1
Shovan Banik <53087831+Shovan-Banik@users.noreply.github.com>
2023-03-28 20:07:55 +0600

    set data from local storage

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,16 +7,30 @@ const Cart = ({cart}) => {
     console.log(cart);
     let totalPrice=0;
     let totalShipping=0;
+    let quantity=0;
     for(const product of cart){
-        totalPrice=totalPrice+product.price;
-        totalShipping=totalShipping+product.shipping;
+        // system 1: 
+        /*  
+        if(product.quantity===0){
+            product.quantity=1;
+        }
+        */
+        
+        // system 2: 
+        /* 
+         product.quantity=product.quantity || 1; 
+
+        */
+        totalPrice=totalPrice+product.price*product.quantity;
+        totalShipping=totalShipping+product.shipping*product.quantity  ;
+        quantity=quantity+product.quantity;
     }
     const tax=totalPrice*7/100;
     const grandTotal=totalPrice+totalShipping+tax;
     return (
         <div className='cart'>
             <h5>Order summary</h5>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price:$ {totalPrice.toFixed(2)}</p>
             <p>Total Shipping:$ {totalShipping.toFixed(2)}</p>
             <p>Tax:$ {tax.toFixed(2)}</p>
