837a25dc9b216d51da799db590b0384ea9a1e382
Shovan Banik <53087831+Shovan-Banik@users.noreply.github.com>
2023-04-19 04:15:00 +0600

    firebase hosting and firebase authentication done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,18 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import './Login.css'
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../providers/AuthProvider';
 
 const Login = () => {
+    const [show,setShow]=useState(false);
+
+
     const {signIn}=useContext(AuthContext);
+    const navigate=useNavigate();
+    const location=useLocation();
+
+    
+    const from=location.state?.from?.pathname || '/';
 
     const handleLogIn=(event)=>{
         event.preventDefault();
@@ -18,6 +26,7 @@ const Login = () => {
             const loggedUser=result.user;
             console.log(loggedUser);
             form.reset();
+            navigate(from,{replace: true});
         })
         .catch(error=>{
             console.error(error);
@@ -35,7 +44,12 @@ const Login = () => {
                 </div>
                 <div className='form-control'>
                     <label htmlFor='password'>Password</label>
-                    <input type="password" name="password" id="password" required/>
+                    <input type={show? 'text' : 'password'} name="password" id="password" required/>
+                    <p onClick={()=>setShow(!show)}><small>
+                        {
+                            show ? <span>Hide Password</span> : <span>Show Password</span>
+                        }
+                        </small></p>
                 </div>
                 <input className='btn-submit' type="submit" value="Login" />
             </form>
--- src/components/__steps__.js
+++ src/components/__steps__.js
@@ -15,3 +15,26 @@
 * 4. set context value
 * 5. set the provider
 */
+
+/*
+* Hosting
+* One time per PC
+* 1. npm install -g firebase-tools
+* 2. firebase login
+
+for each project one time:
+** firebase init
+** hosting: firebase(up and down arrow) use space bar to select 
+** existing project
+** select the project carefully
+** which project as public directory: dist
+** single page application : yes
+** continuous deployment: no
+*/
+
+/****
+ *** for every time deploy
+ * 1. npm run build
+ * 2. firebase deploy
+ * 
+ */
